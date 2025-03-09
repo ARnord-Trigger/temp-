ab0e58341df9dc7f09adc107d6db9db7dbe86319
Anvesh <anvesh9113>
2025-03-09 21:26:10 +0530

    intit

---
+++ t1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define B begin()
+#define E end()
+#define pb push_back
+#define ld long double
+#define f first
+#define s second
+#define pii pair<int, int>
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define rev_all(x) (x).rbegin(), (x).rend() // reverse iterator
+#define rep(i, n) for (int i = 0; i < (n);
+#define mp(x, y) make_pair(x, y)
+void vec_cin(int m, vector<int> &v)
+{
+    for (int i = 0; i < m; i++)
+    {
+        cin >> v[i];
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t ;cin >> t; 
+    while(t--){
+        int n ; 
+        int m ;
+         cin >> n >> m ;
+        vector<vector<int >> v (m , vector<int> (4)) ;
+        for(int i = 0 ; i <  m ; i++){
+            for(int j = 0 ; j < 4 ; j++){
+                cin >> v[i][j] ;
+            }
+        }
+
+        
+
+    }
+    return 0;
+}
